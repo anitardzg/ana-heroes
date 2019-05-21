35c0963b9eee9d6db349a5d9672539e3f7098fca
anitardzg <alr.2020@outlook.com>
2019-05-21 15:23:28 -0700

    9 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent ],
-  bootstrap:    [ AppComponent ],
-  providers: [HeroService, MessageService]
+  imports: [
+     BrowserModule, 
+     FormsModule,
+     AppRoutingModule
+  ],
+  declarations: [ 
+    AppComponent, 
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
